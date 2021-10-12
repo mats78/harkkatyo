4a99ebf02686992683cd5b23cff8062ff976b442
Matias <matias.kallio@seamk.fi>
2021-10-12 10:48:43 +0300

    lisää muutoksia

--- cart.js
+++ cart.js
@@ -2,6 +2,10 @@ const kangasmäärä1_syöte = document.getElementById('kangasmäärä1_input');
 const kangasmäärä2_syöte = document.getElementById('kangasmäärä2_input');
 const kangasmäärä3_syöte = document.getElementById('kangasmäärä2_input');
 const kangasmäärä4_syöte = document.getElementById('kangasmäärä4_input');
+const metrit1_tuloste = document.getElementById('metrit1');
+const metrit2_tuloste = document.getElementById('metrit2');
+const metrit3_tuloste = document.getElementById('metrit3');
+const metrit4_tuloste = document.getElementById('metrit4');
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
 const button3 = document.getElementById('button3');
@@ -22,6 +26,14 @@ var kangasmäärä1 = 1;
 var kangasmäärä2 = 1;
 var kangasmäärä3 = 1;
 var kangasmäärä4 = 1;
+var kangas1summa = 0;
+var kangas2summa = 0;
+var kangas3summa = 0;
+var kangas4summa = 0;
+var metrit1 = 0;
+var metrit2 = 0;
+var metrit3 = 0;
+var metrit4 = 0;
 
 kangasmäärä1_syöte.addEventListener('input', function(event){
     kangasmäärä1 = parseFloat(kangasmäärä1_syöte.value);  
@@ -40,40 +52,64 @@ kangasmäärä4_syöte.addEventListener('input', function(event){
     //kangasmäärä4_syöte.innerHTML = kangasmäärä4;
 });
 button1.addEventListener("click", function(event){
-    var summa = ( parseFloat(kangas1_hinta) * parseFloat(document.getElementById('kangasmäärä1_input').value) );
+    var pituus = parseFloat(document.getElementById('kangasmäärä1_input').value);
+    var summa = parseFloat(kangas1_hinta) * pituus;
+    metrit1 += pituus;
     kokonaissumma += summa;
-
-    kangas1_summa.innerHTML = summa + "€";
+    kangas1summa += summa;
+    metrit1_tuloste.innerHTML = metrit1 + "m";
+    kangas1_summa.innerHTML = kangas1summa + "€";
     ostoskori_Tuloste.innerHTML = kokonaissumma + "€";
  });
 button2.addEventListener("click", function(event){
-    var summa = ( parseFloat(kangas2_hinta) * parseFloat(document.getElementById('kangasmäärä2_input').value) );
+    var pituus = parseFloat(document.getElementById('kangasmäärä2_input').value);
+    var summa = parseFloat(kangas2_hinta) * pituus;
+    metrit2 += pituus;
     kokonaissumma += summa;
-
-    kangas2_summa.innerHTML = summa + "€";
-    ostoskori_Tuloste.innerHTML = kokonaissumma + "€" ;
+    kangas2summa += summa;
+    metrit2_tuloste.innerHTML = metrit2 + "m";
+    kangas2_summa.innerHTML = kangas2summa + "€";
+    ostoskori_Tuloste.innerHTML = kokonaissumma + "€";
  });
 button3.addEventListener("click", function(event){
-    var summa = ( parseFloat(kangas3_hinta) * parseFloat(document.getElementById('kangasmäärä3_input').value) );
+    var pituus = parseFloat(document.getElementById('kangasmäärä3_input').value);
+    var summa = parseFloat(kangas3_hinta) * pituus;
+    metrit3 += pituus;
     kokonaissumma += summa;
-    
-    kangas3_summa.innerHTML = summa + "€";
+    kangas3summa += summa;
+    metrit3_tuloste.innerHTML = metrit3 + "m";
+    kangas3_summa.innerHTML = kangas3summa + "€";
     ostoskori_Tuloste.innerHTML = kokonaissumma + "€";
  });
 button4.addEventListener("click", function(event){
-    var summa = ( parseFloat(kangas4_hinta) * parseFloat(document.getElementById('kangasmäärä4_input').value) );
+    var pituus = parseFloat(document.getElementById('kangasmäärä4_input').value);
+    var summa = parseFloat(kangas4_hinta) * pituus;
+    metrit4 += pituus;
     kokonaissumma += summa;
-
-    kangas4_summa.innerHTML = summa + "€";
+    kangas4summa += summa;   
+    metrit4_tuloste.innerHTML = metrit4 + "m";
+    kangas4_summa.innerHTML = kangas4summa + "€";
     ostoskori_Tuloste.innerHTML = kokonaissumma + "€";
  });
  tyhjää_button.addEventListener("click", function(event){
     kokonaissumma = 0;
+    kangas1summa = 0;
+    kangas2summa = 0;
+    kangas3summa = 0;
+    kangas4summa = 0;
+    metrit1 = 0;
+    metrit2 = 0;
+    metrit3 = 0;
+    metrit4 = 0;
     ostoskori_Tuloste.innerHTML = kokonaissumma + "€";
     kangas1_summa.innerHTML = "0€";
     kangas2_summa.innerHTML = "0€";
     kangas3_summa.innerHTML = "0€";
     kangas4_summa.innerHTML = "0€";
+    metrit1_tuloste.innerHTML = "0m";
+    metrit2_tuloste.innerHTML = "0m";
+    metrit3_tuloste.innerHTML = "0m";
+    metrit4_tuloste.innerHTML = "0m";
  });
 
 // Localstorage
@@ -93,6 +129,6 @@ window.onload = function() {
     ostoskori_Tuloste.innerHTML = kokonaissumma + "€";     
 };
 
-//korjaa viewport muutos
+//korjaa viewport muutos kun kännykän näppäimistö näkyvissä
 var viewport = document.querySelector("meta[name=viewport]");
 viewport.setAttribute("content", viewport.content + ", height=" + window.innerHeight);
